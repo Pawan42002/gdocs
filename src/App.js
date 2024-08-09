c64902d5627c7ff5ad15cb6f13b7dd0e5336fa9f
Pawan42002 <91836724+Pawan42002@users.noreply.github.com>
2024-08-09 12:03:56 +0530

    install react-quill

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TextAreaComponent from "./Funcs/TextAreaComponent";
+import TextEditor from "./Funcs/TextEditor";
 function App() {
 	return (
 		<>
-			<TextAreaComponent />
+			<TextEditor />
 		</>
 	);
 }
